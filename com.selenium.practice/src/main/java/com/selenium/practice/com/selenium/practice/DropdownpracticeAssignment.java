164c6fb31e9cbaeac2fd6241edb5769693fa2cf7
madhubojja90 <madhubojja90@gmail.com>
2026-04-02 00:00:41 +0530

    WebDriverWait example

--- com.selenium.practice/src/main/java/com/selenium/practice/com/selenium/practice/DropdownpracticeAssignment.java
+++ com.selenium.practice/src/main/java/com/selenium/practice/com/selenium/practice/DropdownpracticeAssignment.java
@@ -1,4 +1,5 @@
 package com.selenium.practice.com.selenium.practice;
+
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -10,26 +11,24 @@ import org.openqa.selenium.support.ui.Select;
 public class DropdownpracticeAssignment {
 
 	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub	
-		
-		WebDriver driver=new ChromeDriver();
+		// TODO Auto-generated method stub
+
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 		Thread.sleep(3000);
 		driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
 		System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
-		//count check boxes
-		int size=driver.findElements(By.cssSelector("input[type=\"checkbox\"]")).size();
+		// count check boxes
+		int size = driver.findElements(By.cssSelector("input[type=\"checkbox\"]")).size();
 		System.out.println(size);
-		
+
 		driver.findElement(By.id("autosuggest")).sendKeys("ind");
 		Thread.sleep(3000);
-		List<WebElement> options=driver.findElements(By.xpath("//li[@class='ui-menu-item'] //a"));
-		for(WebElement option :options)
-		{
-			if(option.getText().equalsIgnoreCase("India"))
-			{
-			option.click();
-			break;
+		List<WebElement> options = driver.findElements(By.xpath("//li[@class='ui-menu-item'] //a"));
+		for (WebElement option : options) {
+			if (option.getText().equalsIgnoreCase("India")) {
+				option.click();
+				break;
 			}
 		}
 		driver.quit();
---
+++ com.selenium.practice/src/main/java/com/selenium/practice/com/selenium/practice/LoginPagePractiseWithWaits.java
@@ -0,0 +1,50 @@
+package com.selenium.practice.com.selenium.practice;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPagePractiseWithWaits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+        driver.findElement(By.id("username")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.id("password")).sendKeys("Learning@830$3mK2");
+        List<WebElement> radios = driver.findElements(By.name("radio"));
+        driver.findElement(By.xpath("//input[@type=\"radio\" and @value=\"user\"]")).click();
+        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(20));
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("okayBtn")));
+        WebElement okButton = wait.until(
+        	    ExpectedConditions.elementToBeClickable(By.id("okayBtn"))
+        	);
+        	okButton.click();
+        	Select options=new Select(driver.findElement(By.cssSelector("select[class=\"form-control\"]")));
+        	options.selectByVisibleText("Consultant");
+        	driver.findElement(By.cssSelector("input[id=\"terms\"]")).click();
+        	driver.findElement(By.cssSelector("input[id=\"signInBtn\"]")).click();
+        	wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("button[class=\"btn btn-info\"]")));
+        List<WebElement> addbuttons=driver.findElements(By.cssSelector("button[class=\"btn btn-info\"]"));
+        for(WebElement addbutton:addbuttons)
+        {
+        	addbutton.click();
+        }
+        driver.findElement(By.cssSelector("a[class=\"nav-link btn btn-primary\"]")).click();
+        driver.findElement(By.cssSelector("button[class=\"btn btn-success\"]")).click();
+        driver.findElement(By.id("country")).sendKeys("india");
+        WebElement country=wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[class=\"suggestions\"]")));
+        country.click();
+        	
+        	
+	}
+}
